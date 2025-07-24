f914a87c569da6549ffb740793756ffa1f8ec626
Olexandr Smirnov <olexsmir@gmail.com>
2025-07-24 23:08:23 +0300

    build the image in actions
    apes together strong
    
    i'm an ape
    
    i'm a stupid ape
    
    i'm starting to f ing hate github
    
    will it fix it
    
    i wanna be able to trigger the actions

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -eou pipefail
 
-IMAGE_NAME="ghcr.io/olexsmir/freshrss-image"
-TAG="${1:-latest}"
+IMAGE_NAME="${1:?Missing image name}"
+TAG="${2:-latest}"
 
 echo "[!] Building image $IMAGE_NAME:$TAG"
 docker build -f Containerfile -t "$IMAGE_NAME:$TAG" .
